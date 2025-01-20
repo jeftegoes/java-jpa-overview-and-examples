7f82e43a0c001cb5afffd77a7ff1bd62032c656f
jeftegoes <jefte.goes.dev@outlook.com>
2025-01-20 20:51:07 -0300

    Adding example using Liquibase.

---
+++ Examples/liquibase-basic/src/main/java/com/example/liquibase_basic/LiquibaseBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibase_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseBasicApplication.class, args);
+	}
+
+}
